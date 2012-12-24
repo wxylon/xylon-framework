65d230ff1254231a6249daa8e46b43286a4d2e04
wxylon@gmail.com <wxylon@gmail.com@c7e66e47-5142-0cfe-a31f-f8352d19ca51>
2012-12-24 10:12:41 +0000

    cglib-2.2.2,源码包和测试包，已经cglib依赖包，都已导入，

---
+++ xylon-framework-cglib/src/test/java/com/xylon/cglib/oschinademo/MyMethodInterceptor.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright(c) 2002-2012, wxylon@gmail.com  All Rights Reserved
+ */
+
+package com.xylon.cglib.oschinademo;
+
+import java.lang.reflect.Method;
+
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+/**
+ * @author wxylon@gmail.com
+ * @date 2012-12-24
+ */
+public class MyMethodInterceptor implements MethodInterceptor {
+
+	public Object intercept(Object object, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+		System.out.println(">>>MethodInterceptor start...");
+		Object result = methodProxy.invokeSuper(object, args);
+		System.out.println(">>>MethodInterceptor ending...");
+		return "hahahh";
+	}
+}
---
+++ xylon-framework-cglib/src/test/java/com/xylon/cglib/oschinademo/Target.java
@@ -0,0 +1,18 @@
+/**
+* Copyright(c) 2002-2012, wxylon@gmail.com  All Rights Reserved
+*/
+
+package com.xylon.cglib.oschinademo;
+
+/**
+ * @author wxylon@gmail.com
+ * @date 2012-12-24
+ */
+public class Target {
+	public String execute() {
+        String message = "----------test()----------";
+        System.out.println(message);
+        return message;
+    }
+}
+
